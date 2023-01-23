1d2a07dac2bbbaed96144e0060a9afb7ec909a23
Bennett McCullough <mccullough.bennett@gmail.com>
2023-01-23 15:44:58 -0700

    Add project files.

---
+++ Mission3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission3
+{
+    class Program
+    {
+        static char[] gameBoard = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static void Main(string[] args)
+        {
+            // Welcome the user to the game
+            SupportingClass sc = new SupportingClass();
+            Console.WriteLine("Lets play Tic-Tac-Toe!");
+            int Player = 1;
+            int numberPicked;
+            int whoWon = 0;
+            string Continue = "";
+            // Create a game board array to store the players’ choices
+            while (whoWon == 0)
+            {
+                Console.WriteLine("Player 1 is X and Player 2 is O");
+                Console.WriteLine(" ");
+                
+                if (Continue == "Y" || Continue == "y")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Player 1 is X and Player 2 is O");
+                }
+                if (Player % 2 == 0) //checking the chance of the player
+                {
+                    Console.WriteLine("Player 2's Turn");
+                }
+                else
+                {
+                    Console.WriteLine("Player 1's Turn");
+                }
+                sc.printBoard(gameBoard);
+                numberPicked = int.Parse(Console.ReadLine());
+                // If user choice is not taken, add it to the game board array
+                if (!(gameBoard[numberPicked] == 'O' || gameBoard[numberPicked] == 'X'))
+                {
+                    if (Player % 2 == 0)
+                    {
+                        gameBoard[numberPicked] = 'O';
+                        Player++;
+                    }
+                    else
+                    {
+                        gameBoard[numberPicked] = 'X';
+                        Player++;
+                    }
+                }
+                // They must pick a new spot if taken
+                else
+                {
+                    Console.WriteLine("Please pick another spot");
+                    Console.WriteLine("Press Y to Continue");
+                    Continue = Console.ReadLine();
+                }
+                // -- call CheckWin Function  --
+                whoWon = sc.checkWinner(gameBoard);
+                Console.Clear();
+            }
+            Console.Clear();// clearing the console
+            sc.printBoard(gameBoard); //  ----- PRINT BOARD FUNCTION  -----
+            if (whoWon == 1)
+            // if flag value is 1 then player 1 wins, otherwise flag value 2 indicates player 2 won
+            //means who played marked last time which has win
+            {
+                Console.WriteLine("Player 1 won!");
+            }
+            else if (whoWon == 2)
+            {
+                Console.WriteLine("Player 2 won!");
+            }
+            else// if flag value is -1 the match will be draw and no one is winner
+            {
+                Console.WriteLine("Draw");
+            }
+        }
+    }
+}
---
+++ Mission3/SupportingClass.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission3
+{
+    class SupportingClass
+    {
+        // Print function returns nothing and prints the board display
+        public void printBoard(char[] boardArray)
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  " + boardArray[1]  + "  |  " + boardArray[2] + "  |  " + boardArray[3] );
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  " + boardArray[4] + "  |  " + boardArray[5] + "  |  " + boardArray[6] );
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  " + boardArray[7] + "  |  " + boardArray[8] + "  |  " + boardArray[9]);
+            Console.WriteLine("     |     |      ");
+        }
+
+        // Private function saves code by returning integer based on player's letter
+        private int checkPlayer (char letter)
+        {
+            if (letter == 'X')
+            {
+                return 1;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+        //Checkwinner function determines if there was a win or if the whole board was filled as a tie
+        public int checkWinner (char[] boardArray)
+        {
+            // Check Horizontal rows for a winner
+            if ( (boardArray[1] == boardArray[2]) && (boardArray[2] == boardArray[3]) )
+            {
+                return checkPlayer(boardArray[1]);
+            }
+            else if ((boardArray[4] == boardArray[5]) && (boardArray[5] == boardArray[6]))
+            {
+                return checkPlayer(boardArray[4]);
+            }
+            else if ((boardArray[7] == boardArray[8]) && (boardArray[8] == boardArray[9]))
+            {
+                return checkPlayer(boardArray[7]);
+            }
+
+            // Check vertical rows
+            else if ((boardArray[1] == boardArray[4]) && (boardArray[4] == boardArray[7]))
+            {
+                return checkPlayer(boardArray[1]);
+            }
+            else if ((boardArray[2] == boardArray[5]) && (boardArray[5] == boardArray[8]))
+            {
+                return checkPlayer(boardArray[2]);
+            }
+            else if ((boardArray[3] == boardArray[6]) && (boardArray[6] == boardArray[9]))
+            {
+                return checkPlayer(boardArray[3]);
+            }
+
+            // Check Diagonal Wins
+            else if ((boardArray[1] == boardArray[5]) && (boardArray[5] == boardArray[9]))
+            {
+                return checkPlayer(boardArray[1]);
+            }
+            else if ((boardArray[3] == boardArray[5]) && (boardArray[5] == boardArray[7]))
+            {
+                return checkPlayer(boardArray[3]);
+            }
+
+            // Return -1 if the board is full with no winner aka a tie
+            else if ( boardArray[1] != '1' && boardArray[2] != '2' && boardArray[3] != '3' && boardArray[4] != '4' && boardArray[5] != '5' && boardArray[6] != '6' && boardArray[7] != '7' && boardArray[8] != '8' && boardArray[9] != '9' )
+            {
+                return -1;
+            }
+            // No winner or tie returns 0
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
